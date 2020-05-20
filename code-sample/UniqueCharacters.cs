f794ac2da924abb9e43a62e4747bfbb0d29e125b
WAC0105 <wac0105@STL42WAC032022.local>
2020-05-20 10:16:00 -0500

    add summary to unique chars method

--- code-sample/UniqueCharacters.cs
+++ code-sample/UniqueCharacters.cs
@@ -5,6 +5,11 @@ namespace code_sample
 {
     public static class UniqueCharacters
     {
+        /// <summary>
+        /// Returns true if incoming string has at least one duplicate character, and false otherwise 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>bool</returns>
         public static bool UniqueChars(string str)
         {
             var seenIt = new Dictionary<char, bool>();
